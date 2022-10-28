082973111ea70c8b51192f719bbee85c671e2c28
Alex Oleynik <alexoleynik74@gmail.com>
2022-10-28 12:44:25 -0500

    new post file, dashboard updated

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const { Post } = require('../models/');
+const withAuth = require('../utils/auth');
+
+router.get('/', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            where: {
+                user_id: req.session.user_id
+            },
+        });
+
+        const post = postData.map((post) => post.get({ plain: true }));
+
+        res.render('dashboard', {
+            layout: 'post',
+            post,
+        });
+
+    } catch (err) {
+        res.redirect('login');
+    }
+});
+
+module.exports = router;
