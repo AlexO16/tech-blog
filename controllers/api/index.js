47c57dea5a796a8fccbc710049329f3844d6dae9
Alex Oleynik <alexoleynik74@gmail.com>
2022-10-28 21:57:51 -0500

    Railway

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const postRoutes = require('./post-routes');
-const cmtRoutes = require('./comment-routes');
+//const cmtRoutes = require('./comment-routes');
 
 router.use('/user', userRoutes);
 router.use('/post', postRoutes);
-router.use('/comment', cmtRoutes);
+//router.use('/comment', cmtRoutes);
 
 module.exports = router;
